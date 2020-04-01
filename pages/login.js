0e85afbd9e84598b0d586e2ab3a46140cc905ca4
Paul Gaumer <mitsudomoe8@gmail.com>
2020-04-01 10:06:53 +0200

    Add forms

--- pages/login.js
+++ pages/login.js
@@ -1,51 +1,55 @@
-import React, { useContext } from 'react';
+import React, { useState } from 'react';
 import Layout from '../components/MyLayout.js';
 import fetch from 'isomorphic-unfetch';
 import Cookies from 'js-cookie';
 import Router from 'next/router';
-import { UserDispatchContext } from '../context/userContextProvider';
 
-const LogIn = (props) => {
-  const userDispatch = useContext(UserDispatchContext);
+const login = async (user) => {
+  const res = await fetch('http://localhost:3000/api/users/sign_in', {
+    method: 'POST',
+    headers: {
+      Accept: 'application/json',
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
+      user: { email: user.email, password: user.password }
+    })
+  });
+  const data = await res.json();
+  console.log(data);
+  Cookies.set('token', data.token, { expires: 1 });
+  Router.push('/');
+};
 
-  const login = async () => {
-    const res = await fetch('http://localhost:3000/api/users/sign_in', {
-      method: 'POST',
-      headers: {
-        Accept: 'application/json',
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify({
-        user: { email: 'paul@test.com', password: 'password' }
-      })
-    });
-    const data = await res.json();
-    console.log(data);
-    Cookies.set('token', data.token, { expires: 1 });
-    Router.push('/');
+const LogInPage = () => {
+  const [user, setUser] = useState({ email: '', password: '' });
 
-    // localStorage.setItem('userToken', data.token);
-
-    // userDispatch({
-    //   type: 'SET_CURRENT_USER',
-    //   payload: data.token
-    // });
-
-    // return {
-    //   examples: data
-    // };
-  };
-
-  const handleClick = (e) => {
-    login();
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    login(user);
   };
 
   return (
     <Layout>
       <h1>Login</h1>
-      <button onClick={handleClick}>Login</button>
+
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          placeholder="email"
+          value={user.email}
+          onChange={(e) => setUser({ ...user, email: e.target.value })}
+        />
+        <input
+          type="text"
+          placeholder="password"
+          value={user.password}
+          onChange={(e) => setUser({ ...user, password: e.target.value })}
+        />
+        <button type="submit">Login</button>
+      </form>
     </Layout>
   );
 };
 
-export default LogIn;
+export default LogInPage;
--- pages/signup.js
+++ pages/signup.js
@@ -1,33 +1,58 @@
+import { useState } from 'react';
 import Layout from '../components/MyLayout.js';
 import fetch from 'isomorphic-unfetch';
 
-export default function SignUp(props) {
-  const signup = async () => {
-    const res = await fetch('http://localhost:3000/api/users', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify({
-        user: { email: 'yuk@test.com', password: 'password' }
-      })
-    });
-    const data = await res.json();
-    console.log(data);
+const signup = async (user) => {
+  const res = await fetch('http://localhost:3000/api/users', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
+      user: {
+        email: user.email,
+        password: user.password
+      }
+    })
+  });
 
-    return {
-      currentUserToken: data
-    };
+  console.log(res.headers.get('Authorization'));
+  const data = await res.json();
+
+  return {
+    currentUserToken: data
   };
+};
 
-  const handleClick = (e) => {
-    signup();
+const SignUpPage = (props) => {
+  const [user, setUser] = useState({ email: '', password: '' });
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    signup(user);
   };
 
   return (
     <Layout>
       <h1>Sign Up</h1>
-      <button onClick={handleClick}>Sign Up</button>
+
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          placeholder="email"
+          value={user.email}
+          onChange={(e) => setUser({ ...user, email: e.target.value })}
+        />
+        <input
+          type="text"
+          placeholder="password"
+          value={user.password}
+          onChange={(e) => setUser({ ...user, password: e.target.value })}
+        />
+        <button type="submit">Sign Up</button>
+      </form>
     </Layout>
   );
-}
+};
+
+export default SignUpPage;
