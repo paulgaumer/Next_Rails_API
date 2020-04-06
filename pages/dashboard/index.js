b9bd87b07a0146cc95cb602512c17be15f0c0a71
Paul Gaumer <mitsudomoe8@gmail.com>
2020-04-06 10:14:14 +0200

    Fix redirect from Dashboard if user is not logged in

--- pages/dashboard/index.js
+++ pages/dashboard/index.js
@@ -1,7 +1,8 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import fetch from 'isomorphic-unfetch';
 import nextCookie from 'next-cookies';
 import dynamic from 'next/dynamic';
+import Router from 'next/router';
 import { getDomain } from '../../utils/subdomain';
 import DashboardShell from '../../components/dashboard/dashboardShell';
 import PodcastDetails from '../../components/dashboard/podcastDetails';
@@ -12,8 +13,14 @@ const Dashboard = ({ initialPodcastInfo, initialDomain, loggedIn }) => {
   const [podcast, setPodcast] = useState(initialPodcastInfo);
   const [domain, setDomain] = useState(initialDomain);
 
+  useEffect(() => {
+    if (!loggedIn) {
+      Router.push('/signin');
+    }
+  }, []);
+
   if (!loggedIn) {
-    return <SigninPage />;
+    return <div></div>;
   } else {
     return (
       <DashboardShell podcast={podcast} currentDomain={domain}>
@@ -21,6 +28,8 @@ const Dashboard = ({ initialPodcastInfo, initialDomain, loggedIn }) => {
       </DashboardShell>
     );
   }
+
+  return <div></div>;
 };
 
 export default Dashboard;
