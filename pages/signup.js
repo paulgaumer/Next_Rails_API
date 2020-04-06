2591aad48215618c07ddac22c21aa1abc1f0f9f0
Paul Gaumer <mitsudomoe8@gmail.com>
2020-04-06 22:54:30 +0200

    Style signup page

--- pages/signup.js
+++ pages/signup.js
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 import Layout from '../components/MyLayout.js';
+import Link from 'next/link';
 import fetch from 'isomorphic-unfetch';
 import Cookies from 'js-cookie';
 import Router from 'next/router';
@@ -25,7 +26,7 @@ const signup = async ({ email, password, subdomain }) => {
 };
 
 const SignUpPage = () => {
-  const [details, setDetails] = useState({
+  const [user, setUser] = useState({
     email: '',
     password: '',
     subdomain: '',
@@ -33,43 +34,132 @@ const SignUpPage = () => {
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    signup(details);
+    signup(user);
   };
   const handleChange = (e) => {
     const key = e.target.name;
     const value = e.target.value;
-    setDetails({ ...details, [key]: value });
+    setUser({ ...user, [key]: value });
   };
 
   return (
-    <Layout>
-      <h1>Sign Up</h1>
+    <div className="flex flex-col justify-center min-h-screen py-12 bg-gray-50 sm:px-6 lg:px-8">
+      <div className="sm:mx-auto sm:w-full sm:max-w-md">
+        <img
+          className="w-auto h-12 mx-auto"
+          src="https://tailwindui.com/img/logos/workflow-mark-on-white.svg"
+          alt="Workflow"
+        />
+        <h2 className="mt-6 text-3xl font-extrabold leading-9 text-center text-gray-900">
+          Create your account
+        </h2>
+      </div>
 
-      <form onSubmit={handleSubmit}>
-        <input
-          type="email"
-          name="email"
-          placeholder="email"
-          value={details.email}
-          onChange={handleChange}
-        />
-        <input
-          type="password"
-          name="password"
-          placeholder="password"
-          value={details.password}
-          onChange={handleChange}
-        />
-        <input
-          type="text"
-          name="subdomain"
-          placeholder="mypodcast"
-          value={details.subdomain}
-          onChange={handleChange}
-        />
-        <button type="submit">Sign Up</button>
-      </form>
-    </Layout>
+      <div className="mt-8 sm:mx-auto sm:w-full sm:max-w-md">
+        <div className="px-4 py-8 bg-white shadow sm:rounded-lg sm:px-10">
+          <form action="#" method="POST" onSubmit={handleSubmit}>
+            <div>
+              <label
+                htmlFor="email"
+                className="block text-sm font-medium leading-5 text-gray-700"
+              >
+                Email address
+              </label>
+              <div className="mt-1 rounded-md shadow-sm">
+                <input
+                  required
+                  id="email"
+                  type="email"
+                  value={user.email}
+                  onChange={(e) => setUser({ ...user, email: e.target.value })}
+                  className="block w-full px-3 py-2 placeholder-gray-400 transition duration-150 ease-in-out border border-gray-300 rounded-md appearance-none focus:outline-none focus:shadow-outline-blue focus:border-blue-300 sm:text-sm sm:leading-5"
+                />
+              </div>
+            </div>
+
+            <div className="mt-6">
+              <label
+                htmlFor="password"
+                className="block text-sm font-medium leading-5 text-gray-700"
+              >
+                Password
+              </label>
+              <div className="mt-1 rounded-md shadow-sm">
+                <input
+                  required
+                  id="password"
+                  type="password"
+                  value={user.password}
+                  onChange={(e) =>
+                    setUser({ ...user, password: e.target.value })
+                  }
+                  className="block w-full px-3 py-2 placeholder-gray-400 transition duration-150 ease-in-out border border-gray-300 rounded-md appearance-none focus:outline-none focus:shadow-outline-blue focus:border-blue-300 sm:text-sm sm:leading-5"
+                />
+              </div>
+            </div>
+            <div className="mt-6">
+              <label
+                htmlFor="password"
+                className="block text-sm font-medium leading-5 text-gray-700"
+              >
+                Choose your personal url
+              </label>
+              <div className="flex mt-1 rounded-md shadow-sm">
+                <input
+                  required
+                  id="password"
+                  type="password"
+                  value={user.password}
+                  onChange={(e) =>
+                    setUser({ ...user, password: e.target.value })
+                  }
+                  className="block w-full px-3 py-2 placeholder-gray-400 transition duration-150 ease-in-out border border-gray-300 appearance-none rounded-l-md focus:outline-none focus:shadow-outline-blue focus:border-blue-300 sm:text-sm sm:leading-5"
+                />
+                <span className="inline-flex items-center px-3 text-gray-500 border border-l-0 border-gray-300 rounded-r-md bg-gray-50 sm:text-sm">
+                  .podwii.xyz
+                </span>
+              </div>
+            </div>
+
+            <div className="flex items-center justify-between mt-6">
+              <div className="flex items-center">
+                <input
+                  id="remember_me"
+                  type="checkbox"
+                  className="w-4 h-4 text-indigo-600 transition duration-150 ease-in-out form-checkbox"
+                />
+                <label
+                  htmlFor="remember_me"
+                  className="block ml-2 text-sm leading-5 text-gray-900"
+                >
+                  Remember me
+                </label>
+              </div>
+
+              <div className="text-sm leading-5">
+                <a
+                  href="/"
+                  className="font-medium text-indigo-600 transition duration-150 ease-in-out hover:text-indigo-500 focus:outline-none focus:underline"
+                >
+                  Forgot your password?
+                </a>
+              </div>
+            </div>
+
+            <div className="mt-6">
+              <span className="block w-full rounded-md shadow-sm">
+                <button
+                  type="submit"
+                  className="flex justify-center w-full px-4 py-2 text-sm font-medium text-white transition duration-150 ease-in-out bg-indigo-600 border border-transparent rounded-md hover:bg-indigo-500 focus:outline-none focus:border-indigo-700 focus:shadow-outline-indigo active:bg-indigo-700"
+                >
+                  Get started
+                </button>
+              </span>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
   );
 };
 
