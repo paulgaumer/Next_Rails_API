fb2f5c53e125343f48d6082f96456662dbaee1ab
Paul Gaumer <mitsudomoe8@gmail.com>
2020-04-20 17:58:15 +0200

    add play icon

--- components/podcastLanding/episodes/episode.js
+++ components/podcastLanding/episodes/episode.js
@@ -62,8 +62,10 @@ const Episode = ({ ep }) => {
       <div className="self-center pl-6" data-name="card-content">
         <h3 className="text-xl font-semibold ">{ep.title}</h3>
         <div className="flex items-center mt-3">
-          <div className="flex items-center justify-center flex-shrink-0 w-16 h-16 bg-gray-500 rounded-full">
-            <p className="pl-2 text-4xl text-center text-white">&#9655;</p>
+          <div className="flex items-center justify-center flex-shrink-0 w-12 h-12 bg-red-400 rounded-full">
+            <svg viewBox="0 0 320.001 320.001" className="pl-2 text-white w-8 h-8" fill="currentColor" stroke="currentColor">
+              <path d="M295.84 146.049l-256-144a16.026 16.026 0 00-15.904.128A15.986 15.986 0 0016 16.001v288a15.986 15.986 0 007.936 13.824A16.144 16.144 0 0032 320.001c2.688 0 5.408-.672 7.84-2.048l256-144c5.024-2.848 8.16-8.16 8.16-13.952s-3.136-11.104-8.16-13.952z"/>
+            </svg>
           </div>
           <p className="pl-4 text-base leading-tight leading-7 text-gray-900">
             {truncateText(ep.description, 290)}
--- components/podcastLanding/episodes/episodesList.js
+++ components/podcastLanding/episodes/episodesList.js
@@ -6,9 +6,11 @@ const EpisodesList = ({ episodes, cover }) => {
   return (
     <div className="mx-auto my-20 max-w-7xl">
       <div className="mx-20">
-        <h2 className="px-2 mb-10 text-3xl font-semibold text-gray-600 border-b border-gray-300 ">
-          All Episodes
-        </h2>
+        <div className="mb-10 border-b border-gray-300">
+          <h2 className="inline-block px-2 text-3xl font-semibold text-gray-600 border-b-2 border-red-400">
+            All Episodes
+          </h2>
+        </div>
         {episodesList.map((ep) => {
           return <Episode ep={ep} key={ep.guid} />;
         })}
