fc8d47e885a10fd982b4555984dd3ae97834c0a6
Paul Gaumer <mitsudomoe8@gmail.com>
2020-04-28 16:15:56 +0200

    Update subscribe list

--- components/podcastLanding/header/subscribe/subscribeList.js
+++ components/podcastLanding/header/subscribe/subscribeList.js
@@ -17,17 +17,27 @@ const SubscribeList = () => {
     <ListContainer className="inline-block px-4 rounded shadow-inner">
       {/* <h3 className="text-lg font-light text-center">Subscribe</h3> */}
       <div className="flex justify-center py-4">
-        <div className="pr-3">
+        <div className="flex items-center pr-4">
           <ApplePodcastIcon width={width} height={height} />
+          <div className="pl-2 text-xs font-thin leading-tight">
+            <p>Apple</p>
+            <p>Podcasts</p>
+          </div>
         </div>
-        <div className="px-3">
+        <div className="flex items-center px-4">
           <GooglePodcastIcon width={width} height={height} />
+          <div className="pl-2 text-xs font-thin leading-tight">
+            <p>Google</p>
+            <p>Podcasts</p>
+          </div>
         </div>
-        <div className="px-3">
+        <div className="flex items-center px-4">
           <SpotifyIcon width={width} height={height} />
+          <span className="pl-2 text-xs font-thin leading-tight">Spotify</span>
         </div>
-        <div className="pl-3">
+        <div className="flex items-center pl-4">
           <RssIcon width={width} height={height} />
+          <span className="pl-2 text-xs font-thin leading-tight">Rss</span>
         </div>
       </div>
     </ListContainer>
