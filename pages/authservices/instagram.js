9e09ca0ea5e1ce27a4bd2d38dd2358dfde60ebf0
Paul Gaumer <mitsudomoe8@gmail.com>
2020-05-01 16:39:42 +0200

    Fix

--- pages/authservices/instagram.js
+++ pages/authservices/instagram.js
@@ -5,14 +5,14 @@ import nextCookie from 'next-cookies';
 
 const InstagramAuth = ({ data }) => {
   useEffect(() => {
-    data.access_token !== undefined ? Router.push('/dashboard/podcast') : null;
+    data === 200 ? Router.push('/dashboard/podcast') : null;
   }, []);
 
   return (
     <div>
       <h2 className="text-lg">Instagram Connection</h2>
       <p className="pt-2">
-        Token: {data !== null ? 'Success' : 'Something went wrong'}
+        Token: {data === 200 ? 'Success' : 'Something went wrong'}
       </p>
     </div>
   );
@@ -25,45 +25,50 @@ InstagramAuth.getInitialProps = async function (ctx) {
   const { query } = ctx;
   const { token } = nextCookie(ctx);
 
+  // Check if Instagram Auth sent back a code in the url
   if (query.code === undefined) {
     return { data: null };
   }
 
-  // const searchParams = new URLSearchParams();
-  // searchParams.set('client_id', process.env.INSTAGRAM_CLIENT_ID);
-  // searchParams.set('client_secret', process.env.INSTAGRAM_CLIENT_SECRET);
-  // searchParams.set('grant_type', 'authorization_code');
-  // searchParams.set('redirect_uri', process.env.INSTAGRAM_OAUTH_REDIRECT);
-  // searchParams.set('code', query.code);
+  // Get Short term token
+  const searchParams = new URLSearchParams();
+  searchParams.set('client_id', process.env.INSTAGRAM_CLIENT_ID);
+  searchParams.set('client_secret', process.env.INSTAGRAM_CLIENT_SECRET);
+  searchParams.set('grant_type', 'authorization_code');
+  searchParams.set('redirect_uri', process.env.INSTAGRAM_OAUTH_REDIRECT);
+  searchParams.set('code', query.code);
 
-  // const shortRes = await fetch('https://api.instagram.com/oauth/access_token', {
-  //   method: 'POST',
-  //   body: searchParams,
-  //   headers: {
-  //     Accept: 'application/json, application/xml, text/plain, text/html, *.*',
-  //     'Content-Type': 'application/x-www-form-urlencoded; charset=utf-8',
-  //   },
-  // });
-  // const shortToken = await shortRes.json();
+  const shortRes = await fetch('https://api.instagram.com/oauth/access_token', {
+    method: 'POST',
+    body: searchParams,
+    headers: {
+      Accept: 'application/json, application/xml, text/plain, text/html, *.*',
+      'Content-Type': 'application/x-www-form-urlencoded; charset=utf-8',
+    },
+  });
+  const shortToken = await shortRes.json();
   // console.log(shortToken);
 
-  // if (shortToken.access_token === undefined) {
-  //   return { data: null };
-  // }
+  if (shortToken.access_token === undefined) {
+    return { data: null };
+  }
 
-  // const longRes = await fetch(
-  //   `https://graph.instagram.com/access_token?grant_type=ig_exchange_token&client_secret=${process.env.INSTAGRAM_CLIENT_SECRET}&access_token=${shortToken.access_token}`
-  // );
-  // const longToken = await longRes.json();
+  // Get long term token
+  const longRes = await fetch(
+    `https://graph.instagram.com/access_token?grant_type=ig_exchange_token&client_secret=${process.env.INSTAGRAM_CLIENT_SECRET}&access_token=${shortToken.access_token}`
+  );
+  const longToken = await longRes.json();
   // console.log(longToken);
 
-  // if (longToken.access_token === undefined) {
-  //   return { data: null };
-  // }
+  if (longToken.access_token === undefined) {
+    return { data: null };
+  }
 
+  // Convert expiration time from seconds to date
   const expirationDate = new Date();
   expirationDate.setUTCSeconds(longToken.expires_in);
 
+  // Save Access Token in DB
   const updates = {
     instagram_access_token: {
       access_token: longToken.access_token,
@@ -88,7 +93,7 @@ InstagramAuth.getInitialProps = async function (ctx) {
     ? console.log('Instagram connection ok')
     : console.log('Instagram connection error');
 
-  // return {
-  //   data: longToken,
-  // };
+  return {
+    data: res.status,
+  };
 };
