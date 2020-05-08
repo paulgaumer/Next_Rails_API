2c0ceea7f41686d088659e6cc700b547cd5212d7
Paul Gaumer <mitsudomoe8@gmail.com>
2020-05-08 18:01:45 +0200

    Change about title

--- components/podcastLanding/about/about.js
+++ components/podcastLanding/about/about.js
@@ -1,21 +1,12 @@
-import React, { useContext } from 'react';
-import { GlobalStateContext } from '../../../context/globalContextProvider';
-import { themeOn, themeOff } from '../../../utils/themeHandlers';
+import React from 'react';
 
 const AboutPodcast = ({ data }) => {
-  const { theme, isThemed } = useContext(GlobalStateContext);
-  const colors = isThemed ? theme.colors : '';
-
   return (
     <section className="mx-auto sm:py-10 md:py-16 max-w-7xl">
       <div className="p-12 bg-white shadow-inner sm:shadow sm:mx-20">
         <div className="mb-10 text-center">
           <h2
-            className={`inline-block px-2 text-2xl font-semibold md:text-3xl ${themeOff(
-              isThemed,
-              'text-red-400'
-            )}`}
-            style={{ color: themeOn(isThemed, colors.primary) }}
+            className={`inline-block px-2 text-xl font-medium md:text-2xl text-gray-600`}
           >
             About us
           </h2>
