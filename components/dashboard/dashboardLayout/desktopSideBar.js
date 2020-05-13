cdf70c1a6af3d9339a9874d2f5b8b6217a21b767
Paul Gaumer <mitsudomoe8@gmail.com>
2020-05-13 23:04:17 +0200

    Add support and active tab style

--- components/dashboard/dashboardLayout/desktopSideBar.js
+++ components/dashboard/dashboardLayout/desktopSideBar.js
@@ -1,10 +1,13 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Link from 'next/link';
 import fetch from 'isomorphic-unfetch';
-import Router from 'next/router';
+import Router, { useRouter } from 'next/router';
 import cookie from 'js-cookie';
 
 const DesktopSideBar = ({ podcastSubdomain, currentDomain }) => {
+  const router = useRouter();
+  const [currentPath] = useState(router.pathname);
+
   const handleLogOut = (e) => {
     const token = cookie.get('token');
     const apiUrl = process.env.API_HOST;
@@ -34,7 +37,13 @@ const DesktopSideBar = ({ podcastSubdomain, currentDomain }) => {
           {/* Sidebar component, swap this element with another sidebar if you like */}
           <nav className="flex-1 px-2 mt-5 bg-indigo-800">
             <Link href="/dashboard">
-              <a className="flex items-center px-2 py-2 text-sm font-medium leading-5 text-white transition duration-150 ease-in-out bg-indigo-900 rounded-md group focus:outline-none focus:bg-indigo-700">
+              <a
+                className={`${
+                  currentPath === '/dashboard'
+                    ? 'text-white bg-indigo-900'
+                    : 'mt-1 text-indigo-300 hover:text-white hover:bg-indigo-700 focus:text-white'
+                } group flex items-center px-2 py-2 text-sm leading-5 font-medium rounded-md focus:outline-none focus:bg-indigo-700 transition ease-in-out duration-150`}
+              >
                 <svg
                   className="w-6 h-6 mr-3 text-indigo-400 transition duration-150 ease-in-out group-focus:text-indigo-300"
                   stroke="currentColor"
@@ -52,7 +61,13 @@ const DesktopSideBar = ({ podcastSubdomain, currentDomain }) => {
               </a>
             </Link>
             <Link href="/dashboard/podcast">
-              <a className="flex items-center px-2 py-2 mt-1 text-sm font-medium leading-5 text-indigo-300 transition duration-150 ease-in-out rounded-md group hover:text-white hover:bg-indigo-700 focus:outline-none focus:text-white focus:bg-indigo-700">
+              <a
+                className={`${
+                  currentPath === '/dashboard/podcast'
+                    ? 'text-white bg-indigo-900'
+                    : 'mt-1 text-indigo-300 hover:text-white hover:bg-indigo-700 focus:text-white'
+                } group flex items-center px-2 py-2 text-sm leading-5 font-medium rounded-md focus:outline-none focus:bg-indigo-700 transition ease-in-out duration-150`}
+              >
                 <svg
                   fill="none"
                   stroke="currentColor"
@@ -68,7 +83,13 @@ const DesktopSideBar = ({ podcastSubdomain, currentDomain }) => {
               </a>
             </Link>
             <Link href="/dashboard/crm">
-              <a className="flex items-center px-2 py-2 mt-1 text-sm font-medium leading-5 text-indigo-300 transition duration-150 ease-in-out rounded-md group hover:text-white hover:bg-indigo-700 focus:outline-none focus:text-white focus:bg-indigo-700">
+              <a
+                className={`${
+                  currentPath === '/dashboard/crm'
+                    ? 'text-white bg-indigo-900'
+                    : 'mt-1 text-indigo-300 hover:text-white hover:bg-indigo-700 focus:text-white'
+                } group flex items-center px-2 py-2 text-sm leading-5 font-medium rounded-md focus:outline-none focus:bg-indigo-700 transition ease-in-out duration-150`}
+              >
                 <svg
                   className="w-6 h-6 mr-3 text-indigo-400 transition duration-150 ease-in-out group-hover:text-indigo-300 group-focus:text-indigo-300"
                   stroke="currentColor"
@@ -108,7 +129,7 @@ const DesktopSideBar = ({ podcastSubdomain, currentDomain }) => {
             </div>
           </nav>
         </div>
-        <div className="p-4 border-t border-indigo-700 ">
+        <div className="px-2 py-4">
           <a
             onClick={handleLogOut}
             className="flex items-center px-2 py-2 mt-1 text-sm font-medium leading-5 text-indigo-300 transition duration-150 ease-in-out rounded-md cursor-pointer group hover:text-white hover:bg-indigo-700 focus:outline-none focus:text-white focus:bg-indigo-700"
@@ -124,7 +145,27 @@ const DesktopSideBar = ({ podcastSubdomain, currentDomain }) => {
             >
               <path d="M11 16l-4-4m0 0l4-4m-4 4h14m-5 4v1a3 3 0 01-3 3H6a3 3 0 01-3-3V7a3 3 0 013-3h7a3 3 0 013 3v1"></path>
             </svg>
-            LOG OUT
+            Log Out
+          </a>
+        </div>
+        <div className="px-2 py-1 border-t border-indigo-700">
+          <a
+            href="https://airtable.com/shreUvyZTIzA5LSG6"
+            target="_blank"
+            className="flex items-center px-2 py-2 mt-1 text-sm font-medium leading-5 text-indigo-300 transition duration-150 ease-in-out rounded-md group hover:text-white hover:bg-indigo-700 focus:outline-none focus:text-white focus:bg-indigo-700"
+          >
+            <svg
+              fill="currentColor"
+              viewBox="0 0 20 20"
+              className="w-6 h-6 mr-3 text-indigo-400 transition duration-150 ease-in-out group-hover:text-indigo-300 group-focus:text-indigo-300"
+            >
+              <path
+                d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-2 0c0 .993-.241 1.929-.668 2.754l-1.524-1.525a3.997 3.997 0 00.078-2.183l1.562-1.562C15.802 8.249 16 9.1 16 10zm-5.165 3.913l1.58 1.58A5.98 5.98 0 0110 16a5.976 5.976 0 01-2.516-.552l1.562-1.562a4.006 4.006 0 001.789.027zm-4.677-2.796a4.002 4.002 0 01-.041-2.08l-.08.08-1.53-1.533A5.98 5.98 0 004 10c0 .954.223 1.856.619 2.657l1.54-1.54zm1.088-6.45A5.974 5.974 0 0110 4c.954 0 1.856.223 2.657.619l-1.54 1.54a4.002 4.002 0 00-2.346.033L7.246 4.668zM12 10a2 2 0 11-4 0 2 2 0 014 0z"
+                clipRule="evenodd"
+                fillRule="evenodd"
+              ></path>
+            </svg>
+            Support
           </a>
         </div>
       </div>
