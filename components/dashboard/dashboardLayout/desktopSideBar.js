ccd239cb101fb8b4b9e520467f70ea27e1a076a6
Paul Gaumer <mitsudomoe8@gmail.com>
2020-05-14 18:44:13 +0200

    Create Support page

--- components/dashboard/dashboardLayout/desktopSideBar.js
+++ components/dashboard/dashboardLayout/desktopSideBar.js
@@ -149,24 +149,28 @@ const DesktopSideBar = ({ podcastSubdomain, currentDomain }) => {
           </a>
         </div>
         <div className="px-2 py-1 border-t border-indigo-700">
-          <a
-            href="https://airtable.com/shreUvyZTIzA5LSG6"
-            target="_blank"
-            className="flex items-center px-2 py-2 mt-1 text-sm font-medium leading-5 text-indigo-300 transition duration-150 ease-in-out rounded-md group hover:text-white hover:bg-indigo-700 focus:outline-none focus:text-white focus:bg-indigo-700"
-          >
-            <svg
-              fill="currentColor"
-              viewBox="0 0 20 20"
-              className="w-6 h-6 mr-3 text-indigo-400 transition duration-150 ease-in-out group-hover:text-indigo-300 group-focus:text-indigo-300"
+          <Link href="/dashboard/support">
+            <a
+              className={`${
+                currentPath === '/dashboard/support'
+                  ? 'text-white bg-indigo-900'
+                  : 'mt-1 text-indigo-300 hover:text-white hover:bg-indigo-700 focus:text-white'
+              } group flex items-center px-2 py-2 text-sm leading-5 font-medium rounded-md focus:outline-none focus:bg-indigo-700 transition ease-in-out duration-150`}
             >
-              <path
-                d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-2 0c0 .993-.241 1.929-.668 2.754l-1.524-1.525a3.997 3.997 0 00.078-2.183l1.562-1.562C15.802 8.249 16 9.1 16 10zm-5.165 3.913l1.58 1.58A5.98 5.98 0 0110 16a5.976 5.976 0 01-2.516-.552l1.562-1.562a4.006 4.006 0 001.789.027zm-4.677-2.796a4.002 4.002 0 01-.041-2.08l-.08.08-1.53-1.533A5.98 5.98 0 004 10c0 .954.223 1.856.619 2.657l1.54-1.54zm1.088-6.45A5.974 5.974 0 0110 4c.954 0 1.856.223 2.657.619l-1.54 1.54a4.002 4.002 0 00-2.346.033L7.246 4.668zM12 10a2 2 0 11-4 0 2 2 0 014 0z"
-                clipRule="evenodd"
-                fillRule="evenodd"
-              ></path>
-            </svg>
-            Support
-          </a>
+              <svg
+                fill="currentColor"
+                viewBox="0 0 20 20"
+                className="w-6 h-6 mr-3 text-indigo-400 transition duration-150 ease-in-out group-hover:text-indigo-300 group-focus:text-indigo-300"
+              >
+                <path
+                  d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-2 0c0 .993-.241 1.929-.668 2.754l-1.524-1.525a3.997 3.997 0 00.078-2.183l1.562-1.562C15.802 8.249 16 9.1 16 10zm-5.165 3.913l1.58 1.58A5.98 5.98 0 0110 16a5.976 5.976 0 01-2.516-.552l1.562-1.562a4.006 4.006 0 001.789.027zm-4.677-2.796a4.002 4.002 0 01-.041-2.08l-.08.08-1.53-1.533A5.98 5.98 0 004 10c0 .954.223 1.856.619 2.657l1.54-1.54zm1.088-6.45A5.974 5.974 0 0110 4c.954 0 1.856.223 2.657.619l-1.54 1.54a4.002 4.002 0 00-2.346.033L7.246 4.668zM12 10a2 2 0 11-4 0 2 2 0 014 0z"
+                  clipRule="evenodd"
+                  fillRule="evenodd"
+                ></path>
+              </svg>
+              Support
+            </a>
+          </Link>
         </div>
       </div>
     </div>
