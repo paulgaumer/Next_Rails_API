4cb22cd25358db052954c8bd40e5a434d897e9a2
Paul Gaumer <mitsudomoe8@gmail.com>
2020-05-17 15:31:41 +0200

    Fix Insta Env Prod

--- next.config.js
+++ next.config.js
@@ -3,8 +3,8 @@ module.exports = {
   env: {
     // Reference a variable that was defined in the .env file and make it available at Build Time
     API_HOST: process.env.API_HOST,
-    INSTAGRAM_CLIENT_ID=process.env.INSTAGRAM_CLIENT_ID,
-    INSTAGRAM_CLIENT_SECRET=process.env.INSTAGRAM_CLIENT_SECRET,
-    INSTAGRAM_OAUTH_REDIRECT=process.env.INSTAGRAM_OAUTH_REDIRECT
-  }
+    INSTAGRAM_CLIENT_ID: process.env.INSTAGRAM_CLIENT_ID,
+    INSTAGRAM_CLIENT_SECRET: process.env.INSTAGRAM_CLIENT_SECRET,
+    INSTAGRAM_OAUTH_REDIRECT: process.env.INSTAGRAM_OAUTH_REDIRECT,
+  },
 };
