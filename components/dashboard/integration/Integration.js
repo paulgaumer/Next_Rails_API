7617f38fac7a801e4e2cc6acc00e1f90d9e73b67
Paul Gaumer <mitsudomoe8@gmail.com>
2020-05-21 15:56:33 +0200

    Change style Insta btn dash

--- components/dashboard/integration/Integration.js
+++ components/dashboard/integration/Integration.js
@@ -73,7 +73,7 @@ const IntegrationSection = ({ podcastData }) => {
                     <span className="inline-flex rounded-md shadow-sm">
                       <button
                         type="button"
-                        className="inline-flex items-center px-3 py-2 text-sm font-medium leading-4 text-gray-700 transition duration-150 ease-in-out bg-white border border-gray-300 rounded-md hover:text-gray-500 focus:outline-none focus:border-blue-300 focus:shadow-outline-blue active:text-gray-800 active:bg-gray-50"
+                        className="inline-flex items-center px-3 py-2 text-sm font-medium leading-4 text-indigo-700 transition duration-150 ease-in-out bg-indigo-100 border border-transparent rounded-md hover:bg-indigo-50 focus:outline-none focus:border-indigo-300 focus:shadow-outline-indigo active:bg-indigo-200"
                       >
                         <a
                           href={`https://api.instagram.com/oauth/authorize?client_id=${instaClientId}&redirect_uri=${instaOauthRedirect}&scope=user_profile,user_media&response_type=code`}
