2a14f0243b9900487265cb28966e8072e11cf2fa
Paul Gaumer <mitsudomoe8@gmail.com>
2020-05-25 11:05:14 +0200

    change sidebar to blue

--- components/dashboard/dashboardLayout/mobileSideBar.js
+++ components/dashboard/dashboardLayout/mobileSideBar.js
@@ -51,7 +51,7 @@ const MobileSideBar = ({
           data-todo-x-transition-enter-end="translate-x-0"
           data-todo-x-transition-leave-start="translate-x-0"
           data-todo-x-transition-leave-end="-translate-x-full"
-          className="flex flex-col flex-1 w-full max-w-xs duration-300 ease-in-out transform bg-indigo-800 "
+          className="flex flex-col flex-1 w-full max-w-xs duration-300 ease-in-out transform bg-blue-800 "
         >
           <div
             className="absolute top-0 right-0 p-1 -mr-14"
@@ -89,17 +89,17 @@ const MobileSideBar = ({
                 />
               </a>
             </div>
-            <nav className="flex-1 px-2 mt-5 bg-indigo-800">
+            <nav className="flex-1 px-2 mt-5 bg-blue-800">
               <Link href="/dashboard">
                 <a
                   className={`${
                     currentPath === '/dashboard'
-                      ? 'text-white bg-indigo-900'
-                      : 'mt-1 text-indigo-300 hover:text-white hover:bg-indigo-700 focus:text-white'
-                  } group flex items-center px-2 py-2 text-sm leading-5 font-medium rounded-md focus:outline-none focus:bg-indigo-700 transition ease-in-out duration-150`}
+                      ? 'text-white bg-blue-900'
+                      : 'mt-1 text-blue-300 hover:text-white hover:bg-blue-700 focus:text-white'
+                  } group flex items-center px-2 py-2 text-sm leading-5 font-medium rounded-md focus:outline-none focus:bg-blue-700 transition ease-in-out duration-150`}
                 >
                   <svg
-                    className="w-6 h-6 mr-3 text-indigo-400 transition duration-150 ease-in-out group-focus:text-indigo-300"
+                    className="w-6 h-6 mr-3 text-blue-400 transition duration-150 ease-in-out group-focus:text-blue-300"
                     stroke="currentColor"
                     fill="none"
                     viewBox="0 0 24 24"
@@ -118,9 +118,9 @@ const MobileSideBar = ({
                 <a
                   className={`${
                     currentPath === '/dashboard/podcast'
-                      ? 'text-white bg-indigo-900'
-                      : 'mt-1 text-indigo-300 hover:text-white hover:bg-indigo-700 focus:text-white'
-                  } group flex items-center px-2 py-2 text-sm leading-5 font-medium rounded-md focus:outline-none focus:bg-indigo-700 transition ease-in-out duration-150`}
+                      ? 'text-white bg-blue-900'
+                      : 'mt-1 text-blue-300 hover:text-white hover:bg-blue-700 focus:text-white'
+                  } group flex items-center px-2 py-2 text-sm leading-5 font-medium rounded-md focus:outline-none focus:bg-blue-700 transition ease-in-out duration-150`}
                 >
                   <svg
                     fill="none"
@@ -129,7 +129,7 @@ const MobileSideBar = ({
                     strokeLinejoin="round"
                     strokeWidth="2"
                     viewBox="0 0 24 24"
-                    className="w-6 h-6 mr-3 text-indigo-400 transition duration-150 ease-in-out group-hover:text-indigo-300 group-focus:text-indigo-300"
+                    className="w-6 h-6 mr-3 text-blue-400 transition duration-150 ease-in-out group-hover:text-blue-300 group-focus:text-blue-300"
                   >
                     <path d="M19 11a7 7 0 01-7 7m0 0a7 7 0 01-7-7m7 7v4m0 0H8m4 0h4m-4-8a3 3 0 01-3-3V5a3 3 0 116 0v6a3 3 0 01-3 3z"></path>
                   </svg>
@@ -140,12 +140,12 @@ const MobileSideBar = ({
                 <a
                   className={`${
                     currentPath === '/dashboard/crm'
-                      ? 'text-white bg-indigo-900'
-                      : 'mt-1 text-indigo-300 hover:text-white hover:bg-indigo-700 focus:text-white'
-                  } group flex items-center px-2 py-2 text-sm leading-5 font-medium rounded-md focus:outline-none focus:bg-indigo-700 transition ease-in-out duration-150`}
+                      ? 'text-white bg-blue-900'
+                      : 'mt-1 text-blue-300 hover:text-white hover:bg-blue-700 focus:text-white'
+                  } group flex items-center px-2 py-2 text-sm leading-5 font-medium rounded-md focus:outline-none focus:bg-blue-700 transition ease-in-out duration-150`}
                 >
                   <svg
-                    className="w-6 h-6 mr-3 text-indigo-400 transition duration-150 ease-in-out group-hover:text-indigo-300 group-focus:text-indigo-300"
+                    className="w-6 h-6 mr-3 text-blue-400 transition duration-150 ease-in-out group-hover:text-blue-300 group-focus:text-blue-300"
                     stroke="currentColor"
                     fill="none"
                     viewBox="0 0 24 24"
@@ -164,9 +164,9 @@ const MobileSideBar = ({
                 <a
                   className={`${
                     currentPath === '/dashboard/integration'
-                      ? 'text-white bg-indigo-900'
-                      : 'mt-1 text-indigo-300 hover:text-white hover:bg-indigo-700 focus:text-white'
-                  } group flex items-center px-2 py-2 text-sm leading-5 font-medium rounded-md focus:outline-none focus:bg-indigo-700 transition ease-in-out duration-150`}
+                      ? 'text-white bg-blue-900'
+                      : 'mt-1 text-blue-300 hover:text-white hover:bg-blue-700 focus:text-white'
+                  } group flex items-center px-2 py-2 text-sm leading-5 font-medium rounded-md focus:outline-none focus:bg-blue-700 transition ease-in-out duration-150`}
                 >
                   <svg
                     fill="none"
@@ -175,7 +175,7 @@ const MobileSideBar = ({
                     strokeWidth="2"
                     stroke="currentColor"
                     viewBox="0 0 24 24"
-                    className="w-6 h-6 mr-3 text-indigo-400 transition duration-150 ease-in-out group-hover:text-indigo-300 group-focus:text-indigo-300"
+                    className="w-6 h-6 mr-3 text-blue-400 transition duration-150 ease-in-out group-hover:text-blue-300 group-focus:text-blue-300"
                   >
                     <path d="M17 14v6m-3-3h6M6 10h2a2 2 0 002-2V6a2 2 0 00-2-2H6a2 2 0 00-2 2v2a2 2 0 002 2zm10 0h2a2 2 0 002-2V6a2 2 0 00-2-2h-2a2 2 0 00-2 2v2a2 2 0 002 2zM6 20h2a2 2 0 002-2v-2a2 2 0 00-2-2H6a2 2 0 00-2 2v2a2 2 0 002 2z"></path>
                   </svg>
@@ -183,11 +183,11 @@ const MobileSideBar = ({
                 </a>
               </Link>
 
-              <div className="mt-2 border-t border-indigo-700">
+              <div className="mt-2 border-t border-blue-700">
                 <a
                   href={`http://${podcastSubdomain}.${currentDomain}`}
                   target="_blank"
-                  className="flex items-center px-2 py-2 mt-1 text-sm font-medium leading-5 text-indigo-300 transition duration-150 ease-in-out rounded-md group hover:text-white hover:bg-indigo-700 focus:outline-none focus:text-white focus:bg-indigo-700"
+                  className="flex items-center px-2 py-2 mt-1 text-sm font-medium leading-5 text-blue-300 transition duration-150 ease-in-out rounded-md group hover:text-white hover:bg-blue-700 focus:outline-none focus:text-white focus:bg-blue-700"
                 >
                   <svg
                     fill="none"
@@ -196,7 +196,7 @@ const MobileSideBar = ({
                     strokeLinejoin="round"
                     strokeWidth="2"
                     viewBox="0 0 24 24"
-                    className="w-6 h-6 mr-3 text-indigo-400 transition duration-150 ease-in-out group-hover:text-indigo-300 group-focus:text-indigo-300"
+                    className="w-6 h-6 mr-3 text-blue-400 transition duration-150 ease-in-out group-hover:text-blue-300 group-focus:text-blue-300"
                   >
                     <path d="M10 6H6a2 2 0 00-2 2v10a2 2 0 002 2h10a2 2 0 002-2v-4M14 4h6m0 0v6m0-6L10 14"></path>
                   </svg>
@@ -208,7 +208,7 @@ const MobileSideBar = ({
           <div className="px-2 py-4">
             <a
               onClick={handleLogOut}
-              className="flex items-center px-2 py-2 mt-1 text-sm font-medium leading-5 text-indigo-300 transition duration-150 ease-in-out rounded-md cursor-pointer group hover:text-white hover:bg-indigo-700 focus:outline-none focus:text-white focus:bg-indigo-700"
+              className="flex items-center px-2 py-2 mt-1 text-sm font-medium leading-5 text-blue-300 transition duration-150 ease-in-out rounded-md cursor-pointer group hover:text-white hover:bg-blue-700 focus:outline-none focus:text-white focus:bg-blue-700"
             >
               <svg
                 fill="none"
@@ -217,26 +217,26 @@ const MobileSideBar = ({
                 strokeLinejoin="round"
                 strokeWidth="2"
                 viewBox="0 0 24 24"
-                className="w-6 h-6 mr-3 text-indigo-400 transition duration-150 ease-in-out group-hover:text-indigo-300 group-focus:text-indigo-300"
+                className="w-6 h-6 mr-3 text-blue-400 transition duration-150 ease-in-out group-hover:text-blue-300 group-focus:text-blue-300"
               >
                 <path d="M11 16l-4-4m0 0l4-4m-4 4h14m-5 4v1a3 3 0 01-3 3H6a3 3 0 01-3-3V7a3 3 0 013-3h7a3 3 0 013 3v1"></path>
               </svg>
               Log Out
             </a>
           </div>
-          <div className="px-2 py-1 border-t border-indigo-700">
+          <div className="px-2 py-1 border-t border-blue-700">
             <Link href="/dashboard/support">
               <a
                 className={`${
                   currentPath === '/dashboard/support'
-                    ? 'text-white bg-indigo-900'
-                    : 'mt-1 text-indigo-300 hover:text-white hover:bg-indigo-700 focus:text-white'
-                } group flex items-center px-2 py-2 text-sm leading-5 font-medium rounded-md focus:outline-none focus:bg-indigo-700 transition ease-in-out duration-150`}
+                    ? 'text-white bg-blue-900'
+                    : 'mt-1 text-blue-300 hover:text-white hover:bg-blue-700 focus:text-white'
+                } group flex items-center px-2 py-2 text-sm leading-5 font-medium rounded-md focus:outline-none focus:bg-blue-700 transition ease-in-out duration-150`}
               >
                 <svg
                   fill="currentColor"
                   viewBox="0 0 20 20"
-                  className="w-6 h-6 mr-3 text-indigo-400 transition duration-150 ease-in-out group-hover:text-indigo-300 group-focus:text-indigo-300"
+                  className="w-6 h-6 mr-3 text-blue-400 transition duration-150 ease-in-out group-hover:text-blue-300 group-focus:text-blue-300"
                 >
                   <path
                     d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-2 0c0 .993-.241 1.929-.668 2.754l-1.524-1.525a3.997 3.997 0 00.078-2.183l1.562-1.562C15.802 8.249 16 9.1 16 10zm-5.165 3.913l1.58 1.58A5.98 5.98 0 0110 16a5.976 5.976 0 01-2.516-.552l1.562-1.562a4.006 4.006 0 001.789.027zm-4.677-2.796a4.002 4.002 0 01-.041-2.08l-.08.08-1.53-1.533A5.98 5.98 0 004 10c0 .954.223 1.856.619 2.657l1.54-1.54zm1.088-6.45A5.974 5.974 0 0110 4c.954 0 1.856.223 2.657.619l-1.54 1.54a4.002 4.002 0 00-2.346.033L7.246 4.668zM12 10a2 2 0 11-4 0 2 2 0 014 0z"
