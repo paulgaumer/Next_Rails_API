85ee681eed5b663b3650d1b5e73d79f7dfc4e717
Paul Gaumer <mitsudomoe8@gmail.com>
2020-06-03 13:02:05 +0200

    Add logos to logo cloud

--- components/marketingLanding/components/logo-cloud.js
+++ components/marketingLanding/components/logo-cloud.js
@@ -1,16 +1,11 @@
 import React from 'react';
-// import transistorLogo from '../../../public/marketing/transistor-logo.svg';
-// import buzzsproutLogo from '../../../public/marketing/buzzsprout-logo.svg';
-// import podbeanLogo from '../../../public/marketing/podbean-logo.png';
-// import anchorLogo from '../../../public/marketing/anchor-logo.svg';
-// import soundcloudLogo from '../../../public/marketing/soundcloud-logo.svg';
 
 const LogoCloud = () => {
   return (
     <div id="integration" className="bg-white">
       <div className="max-w-screen-xl px-4 py-12 mx-auto sm:px-6 lg:py-16 lg:px-8">
         <p className="text-base font-semibold leading-6 tracking-wider text-center text-gray-600 uppercase font-titleMarketing">
-          Fast integration with most podcast hosting services
+          Fast integration with a wide range of services
         </p>
         <div className="mt-6 grid grid-cols-2 gap-0.5 md:grid-cols-3 lg:mt-8">
           <div className="flex justify-center col-span-1 px-8 py-8 bg-gray-50">
@@ -46,6 +41,27 @@ const LogoCloud = () => {
               className="max-h-12"
               src="/marketing/soundcloud-logo.svg"
               alt="soundcloud"
+            />
+          </div>
+          <div className="flex items-center justify-center col-span-1 px-6 py-6 bg-gray-50">
+            <img
+              className="max-h-14"
+              src="/marketing/headliner-logo.png"
+              alt="Headliner"
+            />
+          </div>
+          <div className="flex items-center justify-center col-span-1 px-4 py-4 bg-gray-50">
+            <img
+              className="max-h-20"
+              src="/marketing/mailchimp-logo.png"
+              alt="Mailchimp"
+            />
+          </div>
+          <div className="flex items-center justify-center col-span-1 px-4 py-4 bg-gray-50">
+            <img
+              className="max-h-16"
+              src="/marketing/convertkit-logo.png"
+              alt="ConvertKit"
             />
           </div>
           <div className="flex items-center justify-center col-span-1 px-8 py-8 bg-gray-50">
